11bbb881346fffff6588f333c44739265761ac6a
aqilknz <khairunnz123@gmail.com>
2026-06-18 08:37:05 +0700

    feat: implement Login page with Joi validation and Redux integration

---
+++ src/pages/Login.jsx
@@ -0,0 +1,153 @@
+import { useEffect } from 'react';
+import { Link, useNavigate } from 'react-router';
+import joi from 'joi';
+import toast from 'react-hot-toast';
+import { useForm } from 'react-hook-form';
+import { joiResolver } from '@hookform/resolvers/joi';
+import { useDispatch, useSelector } from 'react-redux';
+import Button from '../components/Auth/Button';
+import InputField from '../components/Auth/Input';
+import { loginSlice, clearAuthForce } from '../redux/slices/authSlice';
+
+const schema = joi.object({
+    email: joi.string()
+        .email({ tlds: { allow: false } }).required().messages({
+            "string.empty": "Email is required!",
+            "string.email": "Invalid email format!",
+            "any.required": "Email is required!",
+        }),
+    password: joi.string().min(8).required().messages({
+        "string.empty": "Password is required!",
+        "string.min": "Password must be at least 8 characters!",
+        "any.required": "Password is required!",
+    }),
+});
+
+function Login() {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const { isLoading } = useSelector((state) => state.auth);
+
+    const { register, handleSubmit, formState: { errors } } = useForm({
+        resolver: joiResolver(schema)
+    });
+
+    useEffect(() => {
+        dispatch(clearAuthForce());
+    }, [dispatch]);
+
+    const onSubmit = (data) => {
+        dispatch(loginSlice(data))
+            .unwrap()
+            .then(() => {
+                setTimeout(() => {
+                    toast.success('Login success', {
+                        duration: 5000,
+                        style: {
+                            border: '1px solid #00D452',
+                            padding: '16px',
+                            color: '#00D452',
+                        },
+                        iconTheme: {
+                            primary: '#00D452',
+                            secondary: '#FFFAEE',
+                        },
+                    });
+                    navigate('/dashboard', { replace: true });
+                }, 1000);
+            })
+            .catch((err) => {
+                const errorMessage = typeof err === 'string' ? err : err?.message || "Invalid email or password!";
+
+                // Delay 1 detik untuk toast error
+                setTimeout(() => {
+                    toast.error(errorMessage, {
+                        duration: 5000,
+                    });
+                }, 1000);
+            });
+    };
+
+    return (
+        <div className="font-main relative flex min-h-screen flex-col items-center justify-center px-4 py-8 sm:px-2">
+
+            <h1 className='text-black font-sans font-extrabold text-4xl mb-2'>ShortLink</h1>
+            <main className="md:min-3/6 z-10 w-full max-w-lg rounded-lg bg-white p-8 shadow-lg">
+                <section className="text-center md:text-left mb-4">
+                    <h1 className="text-black text-display-small-bold">
+                        Welcome Back
+                    </h1>
+                    <p className="text-black text-small-normal">
+                        Please enter your details to sign in.
+                    </p>
+                </section>
+
+                <form className="space-y-2" onSubmit={handleSubmit(onSubmit)}>
+                    <InputField
+                        label="Email Address"
+                        type="email"
+                        id="email"
+                        placeholder="name@company.com"
+                        {...register('email')}
+                    />
+                    <div className="h-4 w-full text-left">
+                        {errors.email && (
+                            <p className="text-important text-xsmall-normal">
+                                {errors.email.message}
+                            </p>
+                        )}
+                    </div>
+
+                    <InputField
+                        label="Password"
+                        type="password"
+                        id="password"
+                        placeholder="••••••••"
+                        {...register('password')}
+                    />
+                    <div className="h-4 w-full text-left">
+                        {errors.password && (
+                            <p className="text-important text-xsmall-normal">
+                                {errors.password.message}
+                            </p>
+                        )}
+                    </div>
+
+                    <div className="text-primary cursor-pointer text-right text-sm font-semibold hover:underline">
+                    <Link
+                        to="/auth/forgotpassword"
+                        title="Forgot Password"
+                    >
+                        Forgot Password?
+                    </Link>
+                </div>
+
+                    <Button
+                        type="submit"
+                        color="blue"
+                        size="full"
+                        shape="rectangle"
+                        className={`mt-6 hover:opacity-90 transition-opacity ${isLoading ? 'opacity-50 pointer-events-none' : ''}`}
+                    >
+                        {isLoading ? 'Signing in...' : 'Sign In \u2192'}
+                    </Button>
+                </form>
+
+                <section className="mt-8">
+                    <div className="flex justify-center mt-4 text-center gap-2 text-small-normal text-darkgrey">
+                        Don't have an account?
+                        <Link
+                            to="/auth/register"
+                            title="Sign Up"
+                            className="text-primary text-small-bold cursor-pointer hover:underline"
+                        >
+                            Sign Up
+                        </Link>
+                    </div>
+                </section>
+            </main>
+        </div>
+    );
+}
+
+export default Login;
