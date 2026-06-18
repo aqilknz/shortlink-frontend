82ecf01e3e5720811ff302e7f48c0d316deb4dd3
aqilknz <khairunnz123@gmail.com>
2026-06-18 08:44:38 +0700

    feat: add all route for shortlink app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import { Toaster } from "react-hot-toast";
+import LandingPage from "./pages/LandingPage";
+import Dashboard from "./pages/Dashboard";
+import CreateLink from "./pages/CreateLink";
+import NotFound from "./pages/NotFound";
+import Profile from "./pages/Profile";
+
 function App() {
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-100">
-      <h1 className="text-3xl font-bold text-blue-600 underline">
-        Testing
-      </h1>
-    </div>
-  )
+    <Router>
+      <Toaster position="top-right" reverseOrder={false} />
+      <Routes>
+        {/* Rute Publik */}
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/auth/login" element={<Login />} />
+        <Route path="/auth/register" element={<Register />} />
+
+        <Route element={<ProtectedRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/dashboard/create-link" element={<CreateLink />} />
+          <Route path="/profile" element={<Profile />} />
+        </Route>
+
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
